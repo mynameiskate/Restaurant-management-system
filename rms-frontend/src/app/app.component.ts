51172089b72f701e343486291f0c108eead71540
Natasha01 <borodycknatasha01@mail.ru>
2019-03-27 00:04:13 +0300

    app routing

--- rms-frontend/src/app/app.component.ts
+++ rms-frontend/src/app/app.component.ts
@@ -1,29 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+// import { Observable } from 'rxjs';
 
-import { Dish } from './models/dish.model';
-import { DishService } from './services/dish.service';
-import { appConfig } from './configs/app.config';
+// import { Dish } from './models/dish.model';
+// import { DishService } from './services/dish.service';
+// import { appConfig } from './configs/app.config';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less'],
-  providers: [DishService]
+  styleUrls: ['./app.component.less']//,
+  // providers: [DishService]
 })
 export class AppComponent implements OnInit {
   title = 'rms-frontend';
-  dishes: Dish[] = [];
+  // dishes: Dish[] = [];
 
-  constructor(private dishService: DishService) {
+  constructor(/*private dishService: DishService*/) {
   }
 
-  getImageUrl(imageId) {
-    return appConfig.imageUrl + imageId;
-  }
+  // getImageUrl(imageId) {
+  //   return appConfig.imageUrl + imageId;
+  // }
 
   ngOnInit() {
-    this.dishService.getDishes()
-      .subscribe(res => this.dishes = res);
+    // this.dishService.getDishes()
+    //   .subscribe(res => this.dishes = res);
   }
 }
