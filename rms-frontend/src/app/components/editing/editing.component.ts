1ffc8ea0481914497e7cea7e8113df79030d7461
Natasha01 <borodycknatasha01@mail.ru>
2019-04-02 23:54:56 +0300

    adit aditing component

--- rms-frontend/src/app/components/editing/editing.component.ts
+++ rms-frontend/src/app/components/editing/editing.component.ts
@@ -6,10 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./editing.component.less']
 })
 export class EditingComponent implements OnInit {
+  name: String = '';
+  description: String = '';
+  cost: String = '';
+  weight: String = '';
+  nutritionalValue: String = '';
+  dishCategory: String = '';
+  isAvailable: Boolean = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  checkEnteredData() {
+  }
+
+  fileChange(event) {
+    let fileList: FileList = event.target.files;
+    console.log(fileList);
   }
 
 }
