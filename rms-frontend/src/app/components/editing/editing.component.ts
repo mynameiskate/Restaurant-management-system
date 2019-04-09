2e8742f0d1b3d38ffbb3bb7f9775448e067d4bf7
Natasha01 <borodycknatasha01@mail.ru>
2019-04-10 01:45:38 +0300

    edit menu & editing components

--- rms-frontend/src/app/components/editing/editing.component.ts
+++ rms-frontend/src/app/components/editing/editing.component.ts
@@ -1,9 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { Dish } from '../../models/dish.model';
+import { DishService } from '../../services/dish.service';
+import { appConfig } from '../../configs/app.config';
 
 @Component({
   selector: 'app-editing',
   templateUrl: './editing.component.html',
-  styleUrls: ['./editing.component.less']
+  styleUrls: ['./editing.component.less'],
+  providers: [DishService]
 })
 export class EditingComponent implements OnInit {
   name: String = '';
@@ -14,12 +20,44 @@ export class EditingComponent implements OnInit {
   dishCategory: String = '';
   isAvailable: Boolean = false;
 
-  constructor() { }
+  incorrectData: Boolean = false;
+  operationName: String = 'create';
+
+  constructor(private dishService: DishService) { }
 
   ngOnInit() {
   }
 
+  chooseOperation(event) {
+    this.operationName = event.path[0].name;
+    this.incorrectData = false;
+  }
+
   checkEnteredData() {
+    switch (this.operationName) {
+      case 'create': {
+        console.log('create');
+        if (this.name && this.description && this.cost && this.weight && this.nutritionalValue && this.dishCategory) {
+            this.incorrectData = false;
+            console.log(this.name, this.description, this.cost,this.weight, this.nutritionalValue,this.dishCategory, this.isAvailable);
+            // this.dishService.createDish()
+            //   .subscribe(res => {
+            //     console.log(res);
+            // });
+        } else {
+          this.incorrectData = true;
+        }
+        break;
+      }
+      case 'update': {
+        console.log('update');
+        break;
+      }
+      case 'delete': {
+        console.log('delete');
+        break;
+      }
+    }
   }
 
   fileChange(event) {
